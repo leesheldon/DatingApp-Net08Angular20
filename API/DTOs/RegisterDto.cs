090a3cb9d6ded430e8316e0e69553efa610af8ca
Sheldon <lhanco@gmail.com>
2025-08-22 12:30:47 +0700

    11 - Create reusable text input. Add custom validation. Use form builder service. Create form steps.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -14,4 +14,13 @@ public class RegisterDto
     [Required]
     [MinLength(6)]
     public string Password { get; set; } = "";
+
+    [Required]
+    public string Gender { get; set; } = string.Empty;
+    [Required]
+    public string City { get; set; } = string.Empty;
+    [Required]
+    public string Country { get; set; } = string.Empty;
+    [Required]
+    public DateOnly DateOfBirth { get; set; }
 }
